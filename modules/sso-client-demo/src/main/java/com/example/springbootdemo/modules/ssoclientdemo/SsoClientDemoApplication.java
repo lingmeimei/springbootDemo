58084362b7c84feb9ab71644f262df59e53e27a4
zhangling <zhangl@huaxindata.com.cn>
2018-12-28 16:21:45 +0800

    修改依赖

--- modules/sso-client-demo/src/main/java/com/example/springbootdemo/modules/ssoclientdemo/SsoClientDemoApplication.java
+++ modules/sso-client-demo/src/main/java/com/example/springbootdemo/modules/ssoclientdemo/SsoClientDemoApplication.java
@@ -2,7 +2,14 @@ package com.example.springbootdemo.modules.ssoclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
+/**
+ * @author lengleng
+ * @date 2018年01月27日13:00:09
+ * 单点登录客户端
+ */
+@EnableOAuth2Sso
 @SpringBootApplication
 public class SsoClientDemoApplication {
 
